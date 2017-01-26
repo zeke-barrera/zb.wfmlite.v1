ad7d5a72d6710ea8d9b6bd5e44e4e15724c6e656
Zeke Barrera <ebarrer@amazon.com>
2017-01-25 21:26:46 -0700

    added class methods and created a test file

--- static_engineer_list.py
+++ static_engineer_list.py
@@ -12,3 +12,18 @@ class Engineer:
         self.whid = whid
         self.role = role
         self.bucket = bucket
+    #method for retrieving the engineer's current attributes
+    def get_stats(self):
+        print "Name: %s \t Login: %s \t WHID: %s \t Role: %s \t Bucket: %s" % (self.name, self.login, self.whid, self.role, self.bucket)
+    #the following methods are ways to adjust engineer attributes and will tie in with the GUI
+    def change_name(self, new_name):
+        self.name = new_name
+    
+    def change_whid(self, new_whid):
+        self.whid = new_whid
+        
+    def change_role(self, new_role):
+        self.role = new_role
+        
+    def change_bucket(self, new_bucket):
+        self.bucket = new_bucket
---
+++ test.py
@@ -0,0 +1,18 @@
+'''
+Created on Jan 25, 2017
+
+@author: ebarrer
+'''
+from static_engineer_list import Engineer
+
+ebarrer = Engineer("Zeke Barrera","ebarrer","AZA1","CS","AEN")
+
+ebarrer.get_stats()
+
+ebarrer.change_bucket("High Sevs")
+
+ebarrer.get_stats()
+
+ebarrer.change_name("Ezequiel Barrera")
+
+ebarrer.get_stats()
